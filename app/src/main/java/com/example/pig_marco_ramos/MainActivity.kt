d7617c8e4e274405b221ea24aa2b8deea0352fb8
byMarkky <mo6368745@gmail.com>
2024-10-20 23:59:48 +0200

    Some testing

--- app/src/main/java/com/example/pig_marco_ramos/MainActivity.kt
+++ app/src/main/java/com/example/pig_marco_ramos/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.pig_marco_ramos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.pig_marco_ramos.databinding.ActivityMainBinding
+import kotlin.properties.Delegates
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var ronda = 0
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (ronda >= 5) {
+                binding.text.text = "Se acabo el juego"
+            } else {
+                binding.text.text = (Random.nextInt(0, 6) + 1).toString()
+                ronda += 1
+            }
+
         }
+
     }
 }
