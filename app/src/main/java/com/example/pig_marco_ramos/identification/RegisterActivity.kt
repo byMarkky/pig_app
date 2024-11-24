2fbd111a21790e8ff177706ef6662aed96fae001
byMarkky <mo6368745@gmail.com>
2024-11-24 16:07:36 +0100

    UI Change

--- app/src/main/java/com/example/pig_marco_ramos/identification/RegisterActivity.kt
+++ app/src/main/java/com/example/pig_marco_ramos/identification/RegisterActivity.kt
@@ -1,15 +1,9 @@
 package com.example.pig_marco_ramos.identification
 
-import android.app.DatePickerDialog
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.example.pig_marco_ramos.databinding.ActivityRegisterBinding
 import com.example.pig_marco_ramos.datepicker.DatePickerFragment
@@ -18,9 +12,6 @@ import com.example.pig_marco_ramos.room.User
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.time.LocalDate
-import java.time.Period
 import java.util.Calendar
 
 class RegisterActivity : AppCompatActivity() {
@@ -32,8 +23,7 @@ class RegisterActivity : AppCompatActivity() {
     private var month: Int = 0
     private var year: Int = 0
     private var errors: MutableList<String> = ArrayList(4)
-    private val _userExists = MutableLiveData<Boolean>()
-    private val userExists: LiveData<Boolean> get() = _userExists
+    private val MIN_AGE = 16
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,8 +48,6 @@ class RegisterActivity : AppCompatActivity() {
                 user = binding.registerName.text.toString()
                 pass = binding.registerPassword.text.toString()
 
-                Log.d("NAME", "NAME: $user")
-                Log.d("NAME", "PASS: $pass")
                 val birthDate = binding.registerDate.text.toString()
                 val user = User(name = user, password = pass, birthDate = birthDate)
 
@@ -85,33 +73,37 @@ class RegisterActivity : AppCompatActivity() {
 
         // The user do not accept the terms
         if (!binding.termsCheck.isChecked) {
-            errors.add("Tienes que aceptar los terminos y condiciones")
+            makeSnackBar("You have to accept the terms and conditions")
             return false
         }
 
         if (userName.length < 4) {
-            errors.add("La contraseña debe tener un minimo de 4 y maximo 10 caracteres")
+            makeSnackBar("Name must be between 4 and 10 characters")
             return false
         }
 
         if (!validatePassword(binding.registerPassword.text.toString())) {
-            errors.add("La contraseña tener un minimo de 4 y maximo 10 caracteres")
+            makeSnackBar("Password: 4 - 10 characters and at least 1 digit")
             return false
         }
 
-        if (!validateDate(16)) {
-            errors.add("Debes tener mas de 16 años para registrarte")
+        if (!validateDate()) {
+            makeSnackBar("You have to be 16 y.o. at least")
             return false
         }
 
         val db = AppDatabase.getInstance(this)
         val userDao = db.getDao()
         if (userDao.exist(userName, userPass)) {
-            errors.add("El usuario ya existe, pruebe otro ó logeate")
+            makeSnackBar("This user already exists")
             return false
         }
 
         return true
+    }
+
+    private fun makeSnackBar(text: String) {
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_LONG).show()
     }
 
     private fun validatePassword(passwd: String): Boolean {
@@ -119,10 +111,7 @@ class RegisterActivity : AppCompatActivity() {
         if (passwd.length < 4) return false
 
         // Check if password have at least one number
-        if (passwd.contains(".*\\d.*")) {
-            Log.i("PASSWORD", "THE PASSWORD MUST HAVE AT LEAST ONE NUMBER")
-            return false
-        }
+        if (!passwd.contains("[0-9]".toRegex())) return false
 
         return true
     }
@@ -130,17 +119,16 @@ class RegisterActivity : AppCompatActivity() {
     /**
      * Method that get the current date and parse the selected date
      * to Calendar to evaluate if the user have a valid years
-     * @param minAge Minimum age to be compared
      * @return boolean True if the user age if valid, False if not
      */
-    private fun validateDate(minAge: Int): Boolean {
+    private fun validateDate(): Boolean {
         val currentDate = Calendar.getInstance()
         val selectedDate = Calendar.getInstance()
         val stringDate = binding.registerDate.text.toString().split("/")
         selectedDate.set(stringDate[2].toInt(), stringDate[1].toInt(), stringDate[0].toInt())
         val ageInYears = currentDate.get(Calendar.YEAR) - selectedDate.get(Calendar.YEAR)
 
-        return ageInYears >= minAge
+        return ageInYears >= MIN_AGE
     }
 
     private fun showDatePicker() {
@@ -149,7 +137,6 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun onDateSelected(day: Int, month: Int, year: Int) {
-        Log.i("DATE", "YEAR: $year, MONTH: $month, DAY: $day")
         this.day = day
         this.month = month
         this.year = year
