1281133fe95d5bd4a871d76d299ab14f6f86cbd6
byMarkky <mo6368745@gmail.com>
2025-02-04 10:00:19 +0100

    Audio in Hub Feature

--- app/src/main/java/com/example/pig_marco_ramos/HubActivity.kt
+++ app/src/main/java/com/example/pig_marco_ramos/HubActivity.kt
@@ -1,8 +1,11 @@
 package com.example.pig_marco_ramos
 
 import android.content.Intent
+import android.media.SoundPool
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,10 +21,15 @@ import com.example.pig_marco_ramos.room.User
 class HubActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHubBinding
+    private lateinit var sound: SoundPool
+
+    private var soundPaused = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        sound()
 
         val user = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getSerializableExtra("USER", User::class.java)
@@ -58,6 +66,37 @@ class HubActivity : AppCompatActivity() {
             val intent = Intent(this, VideoPlayerActivity::class.java)
             intent.putExtra("USER", user)
             startActivity(intent)
+        }
+
+        binding.audioButton.setOnClickListener { soundButtonMgr() }
+
+    }
+
+    private fun soundButtonMgr() {
+        if (soundPaused) {
+            sound.autoResume()
+            binding.audioButton.setImageResource(android.R.drawable.ic_media_pause)
+            Log.d("AUDIO", "AUDIO PLAYING")
+        } else {
+            sound.autoPause()
+            binding.audioButton.setImageResource(android.R.drawable.ic_media_play)
+            Log.d("AUDIO", "AUDIO PAUSED")
+        }
+
+        soundPaused = !soundPaused
+    }
+
+    private fun sound() {
+        sound = SoundPool.Builder().build()
+        val soundID = sound.load(this, R.raw.sample_audio, 1)
+        sound.setOnLoadCompleteListener { pool, id, status ->
+            if (status == 0) {
+                val streamID = pool.play(soundID,0.5F,0.5F,1,-1,1F)
+                Log.d("AUDIO", "Audio Loaded Successful")
+            } else Log.d("AUDIO", "STATUS: $status")
+
+            binding.audioButton.setImageResource(android.R.drawable.ic_media_pause)
+            soundPaused = false
         }
 
     }
